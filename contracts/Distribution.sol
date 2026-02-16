adb90e04fbf500f2203c8b2b57b934b29ce32145
malteish <malteish@corpus.io>
2026-02-16 11:22:56 +0100

    prettier

--- contracts/Distribution.sol
+++ contracts/Distribution.sol
@@ -2,7 +2,6 @@
 
 pragma solidity 0.8.23;
 
-
 import "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
 import "@openzeppelin/contracts/interfaces/IERC1271.sol";
 
@@ -10,15 +9,13 @@ import "./Vesting.sol";
 import "./Token.sol";
 import "./Coinvestor.sol";
 
-
 /**
  * @title tokenize.it Distribution
  * @author malteish, cjentzsch
  * @notice This contract implements the distribution of any proceeds (Exit, Liquidation, Dividends) based on a snapshot of Token.sol
- *    
+ *
  */
-contract Distribution is ERC2771ContextUpgradeable, Ownable2StepUpgradeable
-{
+contract Distribution is ERC2771ContextUpgradeable, Ownable2StepUpgradeable {
     Token public token;
     uint public snapshotId;
     uint public totalTokenAmount;
@@ -32,7 +29,8 @@ contract Distribution is ERC2771ContextUpgradeable, Ownable2StepUpgradeable
         exit = _exit;
     }
 
-    function confirm(uint _snapshotId, IERC20 _currency, uint _totalCurrencyAmount) onlyOwner { // once could also rename it to initalized, both work
+    function confirm(uint _snapshotId, IERC20 _currency, uint _totalCurrencyAmount) onlyOwner {
+        // one could also rename it to initalized, both work
         snapshotId = _snapshotId;
         totalTokenAmount = token.totalSupplyAt(snapshotId);
         currency = _currency;
@@ -40,20 +38,21 @@ contract Distribution is ERC2771ContextUpgradeable, Ownable2StepUpgradeable
         totalCurrencyAmount = _totalCurrencyAmount;
     }
 
-    function eligible(address _holder) returns(uint) {
-        return totalCurrencyAmount * token.balanceOfAt(_holder, snapshotId) / totalTokenAmount - paidOut[_holder];
-    } 
+    function eligible(address _holder) returns (uint) {
+        return (totalCurrencyAmount * token.balanceOfAt(_holder, snapshotId)) / totalTokenAmount - paidOut[_holder];
+    }
 
     function claim(address _recipient) {
         _claim(_msgSender(), _recipient); //should work for directly calling it (msg.sender), as well as with a meta transactions with a siged message
     }
 
-    function claim(IERC1271 _holder, bytes32 _hash, bytes memory _signature, address _recipient){
+    function claim(IERC1271 _holder, bytes32 _hash, bytes memory _signature, address _recipient) {
         require(_holder.isValidSignature(_hash, _signature) == 0x1626ba7e);
         _claim(_holder, _recipient);
     }
 
-    function claim(Vesting _holder, address _recipient){ //only works for lockups, where there is only one vesting plan per deployment. For EP it will not and should not work, since there are not tokens in EP contracts
+    function claim(Vesting _holder, address _recipient) {
+        //only works for lockups, where there is only one vesting plan per deployment. For EP it will not and should not work, since there are not tokens in EP contracts
         require(_msgSender() == _holder.vestings[1].beneficiary());
         _claim(_holder, _recipient);
     }
@@ -70,8 +69,8 @@ contract Distribution is ERC2771ContextUpgradeable, Ownable2StepUpgradeable
             currency.transfer(_coinvestor.beneficiaries(0), payoutCoinvestor); // TODO handle case when the payout is lower than the base
         }
 
-        for (uint i=0; i<length; i++) {
-            uint amount = _coinvestor.percentage(i) * totalAmount / type(uint64).max;
+        for (uint i = 0; i < length; i++) {
+            uint amount = (_coinvestor.percentage(i) * totalAmount) / type(uint64).max;
             paidOut[_coinvestor] += amount;
             currency.transfer(_coinvestor.beneficiaries(i), amount);
         }
--- contracts/TokenSwapCarry.sol
+++ contracts/TokenSwapCarry.sol
@@ -47,7 +47,12 @@ struct TokenSwapInitializerArguments {
  *          Then any party wanting to sell tokens can do so through the sell function.
  * @dev The contract inherits from ERC2771Context in order to be usable with Gas Station Network (GSN) https://docs.opengsn.org/faq/troubleshooting.html#my-contract-is-using-openzeppelin-how-do-i-add-gsn-support
  */
-contract TokenSwapCarry is ERC2771ContextUpgradeable, OwnableUpgradeable, PausableUpgradeable, ReentrancyGuardUpgradeable {
+contract TokenSwapCarry is
+    ERC2771ContextUpgradeable,
+    OwnableUpgradeable,
+    PausableUpgradeable,
+    ReentrancyGuardUpgradeable
+{
     using SafeERC20 for IERC20;
 
     /// address that receives the currency/tokens when tokens are bought/sold
@@ -172,8 +177,8 @@ contract TokenSwapCarry is ERC2771ContextUpgradeable, OwnableUpgradeable, Pausab
         // payout rest + carries
         uint length = holder.beneficiaries().length();
 
-        for (uint i=0; i<length; i++) {
-            uint amount = holder.percentage(i) * totalAmount / type(uint64).max;
+        for (uint i = 0; i < length; i++) {
+            uint amount = (holder.percentage(i) * totalAmount) / type(uint64).max;
             paidOut[holder] += amount;
             currency.transfer(holder.beneficiaries(i), amount);
         }
